3926115214bd096d74b37202204faa89a1e17fd3
ONUR ŞAHİN <57417618+derinonursahin@users.noreply.github.com>
2023-07-26 10:04:09 +0300

    add: WillPopScope in Game Screen

--- 5- Libraries/Hero/hero_app/lib/hero_view.dart
+++ 5- Libraries/Hero/hero_app/lib/hero_view.dart
@@ -65,105 +65,110 @@ class _HeroViewState extends State<HeroView>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    for (int i = 0; i < 3; i++)
-                      Icon(
-                        Icons.favorite,
-                        color: i < falseCount ? Colors.red : Colors.grey,
-                      ),
-                  ],
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.red,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      for (int i = 0; i < 3; i++)
+                        Icon(
+                          Icons.favorite,
+                          color: i < falseCount ? Colors.red : Colors.grey,
+                        ),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 50),
-              Expanded(
-                child: Stack(
-                  children: [
-                    if (currentLevel > 0)
+                const SizedBox(height: 50),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      if (currentLevel > 0)
+                        SlideTransition(
+                          position: _slideAnimation,
+                          child: Image.asset(
+                            Levels.entries.elementAt(currentLevel - 1).key,
+                            key: const Key('oldImage'),
+                          ),
+                        ),
                       SlideTransition(
                         position: _slideAnimation,
                         child: Image.asset(
-                          Levels.entries.elementAt(currentLevel - 1).key,
-                          key: const Key('oldImage'),
+                          Levels.entries.elementAt(currentLevel).key,
+                          key: const Key('newImage'),
                         ),
                       ),
-                    SlideTransition(
-                      position: _slideAnimation,
-                      child: Image.asset(
-                        Levels.entries.elementAt(currentLevel).key,
-                        key: const Key('newImage'),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              if (currentLevel < Levels.length - 1)
-                ArrowButtons(
-                  heartStates: List.generate(3, (index) => index < falseCount),
-                  enabledArrowButton:
-                      Levels.entries.elementAt(currentLevel).value,
-                  onArrowPressed: (direction) async {
-                    if (Levels.entries.elementAt(currentLevel).value ==
-                        direction) {
-                      setState(() {
-                        currentLevel++;
-                      });
-                    } else {
-                      setState(() {
-                        falseCount--;
-                      });
-                      if (falseCount == 0) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const GameOverView(),
-                          ),
-                        );
+                const SizedBox(height: 20),
+                if (currentLevel < Levels.length - 1)
+                  ArrowButtons(
+                    heartStates:
+                        List.generate(3, (index) => index < falseCount),
+                    enabledArrowButton:
+                        Levels.entries.elementAt(currentLevel).value,
+                    onArrowPressed: (direction) async {
+                      if (Levels.entries.elementAt(currentLevel).value ==
+                          direction) {
+                        setState(() {
+                          currentLevel++;
+                        });
                       } else {
-                        await _showAlertDialog(context);
+                        setState(() {
+                          falseCount--;
+                        });
+                        if (falseCount == 0) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const GameOverView(),
+                            ),
+                          );
+                        } else {
+                          await _showAlertDialog(context);
+                        }
                       }
-                    }
-                    _updateSlideAnimation();
-                    _controller.forward(from: 0.0);
-                  },
-                ),
-              const SizedBox(height: 50),
-              if (currentLevel == Levels.length - 1)
-                ElevatedButton(
-                  onPressed: () {
-                    falseCount = 3;
-                    Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(
-                        builder: (context) => const GameScreen(),
+                      _updateSlideAnimation();
+                      _controller.forward(from: 0.0);
+                    },
+                  ),
+                const SizedBox(height: 50),
+                if (currentLevel == Levels.length - 1)
+                  ElevatedButton(
+                    onPressed: () {
+                      falseCount = 3;
+                      Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(
+                          builder: (context) => const GameScreen(),
+                        ),
+                        (route) => false,
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
                       ),
-                      (route) => false,
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+                      minimumSize: const Size(200, 60),
                     ),
-                    minimumSize: const Size(200, 60),
+                    child: const Text(
+                      'Restart',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
                   ),
-                  child: const Text(
-                    'Restart',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
