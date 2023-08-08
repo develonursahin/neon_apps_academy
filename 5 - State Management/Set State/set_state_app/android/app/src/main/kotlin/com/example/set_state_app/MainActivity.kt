c6b09eb9e7a588e67edf3bbb0ae1bc492ea57e6c
ONUR ŞAHİN <57417618+derinonursahin@users.noreply.github.com>
2023-08-08 11:43:40 +0300

    add: State Management, Set State

---
+++ 5 - State Management/Set State/set_state_app/android/app/src/main/kotlin/com/example/set_state_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.set_state_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 5 - State Management/Set State/set_state_app/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Set State Counter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SetStateView(title: 'Set State Counter'),
+    );
+  }
+}
+
+class SetStateView extends StatefulWidget {
+  const SetStateView({super.key, required this.title});
+  final String title;
+  @override
+  State<SetStateView> createState() => _SetStateViewState();
+}
+
+class _SetStateViewState extends State<SetStateView> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
